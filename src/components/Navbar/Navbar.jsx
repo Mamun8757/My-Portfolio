909d15c90bf33e0386954ac37889e6845f5467a1
Mamun8757 <almamun8757@gmail.com>
2022-11-25 16:46:15 +0600

    Navbar Setup Done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion';
 
+import { images } from '../../constants';
 import './Navbar.scss'
+
 const Navbar = () => {
     return (
-        <div>
-            <h2>Navbar</h2>
-        </div>
+        <nav className='app__navbar'>
+            <div className='app__navbar-logo'>
+                <img src={images.logo} alt="logo" />
+            </div>
+            <ul className='app__navbar-links'>
+                {['home', 'about', 'skills', 'work', 'contact'].map((item) =>(
+                    <li className='app__flex p-text' key={`link-${item}`}>
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                )) }
+            </ul>
+            <div className='app_navbar-menu'>
+
+            </div>
+        </nav>
     );
 };
 
