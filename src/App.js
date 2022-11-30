136a04c87a3613288a87f1fb6fe71885d037e0eb
Mamun8757 <almamun8757@gmail.com>
2022-12-01 00:32:48 +0600

    Almost Done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { About, Footer, Header, Skills, Work } from './container';
 import { Navbar } from './components';
 import './App.scss';
 const App = () => {
     return (
-        <div className='app'>
-            <Navbar />
-            <Header />
-            <About />
-            <Skills />
-            <Testimonial />
-            <Work />
-            <Footer />
-        </div>
+      <div className="app">
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        {/* <Testimonial /> */}
+        <Footer />
+      </div>
     );
 };
 
